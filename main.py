c366bf15f51ee3831c036260e8dc3d067677369e
PM-95025 <50608884+pm-95025@users.noreply.github.com>
2022-08-07 13:47:56 +0300

    reindent

--- main.py
+++ main.py
@@ -18,13 +18,13 @@ land = rospy.ServiceProxy('land', Trigger)
 
 
 def navigate_wait(x=0, y=0, z=0, yaw=float('nan'), speed=0.5, frame_id='', auto_arm=False, tolerance=0.2):
-    navigate(x=x, y=y, z=z, yaw=yaw, speed=speed, frame_id=frame_id, auto_arm=auto_arm)
-
-    while not rospy.is_shutdown():
-        telem = get_telemetry(frame_id='navigate_target')
-        if math.sqrt(telem.x ** 2 + telem.y ** 2 + telem.z ** 2) < tolerance:
-            break
-        rospy.sleep(0.2)
+	navigate(x=x, y=y, z=z, yaw=yaw, speed=speed, frame_id=frame_id, auto_arm=auto_arm)
+	
+	while not rospy.is_shutdown():
+		telem = get_telemetry(frame_id='navigate_target')
+		if math.sqrt(telem.x ** 2 + telem.y ** 2 + telem.z ** 2) < tolerance:
+			break
+		rospy.sleep(0.2)
 
 
 navigate_wait(z=1, frame_id='body', auto_arm=True)
